d39a18b9f6f238d0bd912b3fba6f31d29407d532
KMcCrear <kmccrear1@gmail.com>
2021-02-24 17:03:11 +0000

    cookie and session

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
 
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
 const bcrypt = require("bcrypt");
 const { response } = require("express");
 const saltRounds = 10;
@@ -9,7 +13,28 @@ const saltRounds = 10;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+	cors({
+		origin: ["https://localhost:3000"],
+		methods: ["GET", "POST"],
+		credentials: true,
+	})
+);
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+	session({
+		key: "userId",
+		secret: "cookieSecret",
+		resave: false,
+		saveUninitialized: false,
+		cookie: {
+			expires: 60 * 60 * 24,
+		},
+	})
+);
 
 const db = mysql.createConnection({
 	user: "root",
@@ -55,6 +80,8 @@ app.post("/login", (req, res) => {
 			if (result.length > 0) {
 				bcrypt.compare(password, result[0].password, (error, response) => {
 					if (response) {
+						req.session.user = result;
+						console.log(req.session.user);
 						res.send(result);
 					} else {
 						res.send({ message: "Wrong Email/Password" });

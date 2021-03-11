4f6f22bdc90de6e7ac92a99f0047acdfe26ff0db
KMcCrear <kmccrear1@gmail.com>
2021-03-11 16:49:17 +0000

    Covid-19 Final

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,54 @@
 import "./App.css";
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
-import NavBar from './components/NavBar';
-import Login from './pages/Login'
-import Test from './pages/Test'
+import NavBar from "./components/NavBar";
+import Login from "./pages/Login";
+import CovidTracker from "./pages/CovidTracker";
 
 class App extends React.Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
-		this.state= {
+		this.state = {
 			loggedIn: false,
 			email: null,
 			firstname: null,
 			surname: null,
 			message: null,
-		}
+		};
 	}
-	onUpdate(object){
+	onUpdate(object) {
 		this.setState(object);
 	}
-	render(){
-	console.log('state is ', this.state)
-	return (
-		<>
-		<div className='App'>
-		<NavBar/>
-		<Router>
-			<Route path="/" exact render={()=><Home state={this.state} onUpdate={this.onUpdate.bind(this)}/>} />
-			<Route path="/register" render={()=><Register state={this.state} onUpdate={this.onUpdate.bind(this)}/>} />
-		</Router>
-		</div>
-		</>
-	);
+	render() {
+		console.log("state is ", this.state);
+		return (
+			<>
+				<div className="App">
+					<NavBar />
+					<Router>
+						<Route
+							path="/"
+							exact
+							render={() => (
+								<Home state={this.state} onUpdate={this.onUpdate.bind(this)} />
+							)}
+						/>
+						<Route
+							path="/register"
+							render={() => (
+								<Register
+									state={this.state}
+									onUpdate={this.onUpdate.bind(this)}
+								/>
+							)}
+						/>
+						<Route path="/covidtracking" render={() => <CovidTracker />} />
+					</Router>
+				</div>
+			</>
+		);
 	}
 }
 

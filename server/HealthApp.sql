61e8789971967322b3887e3ef42b34f53ac47be4
StefanBabukov <stefanbabukov98@gmail.com>
2021-03-18 21:27:16 +0200

    fixed sql (#13)

--- server/HealthApp.sql
+++ server/HealthApp.sql
@@ -3,14 +3,15 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 18, 2021 at 08:04 PM
+-- Generation Time: Feb 23, 2021 at 02:56 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
+CREATE DATABASE IF NOT EXISTS HealthApp;
+USE HealthApp;
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -24,8 +25,35 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `activities`
+-- Table structure for table `users`
 --
+DROP TABLE  IF EXISTS `users`;
+
+CREATE TABLE IF NOT EXISTS`users` (
+  `id` int(11) PRIMARY KEY NOT NULL,
+  `firstname` varchar(512) NOT NULL,
+  `surname` varchar(512) NOT NULL,
+  `email` varchar(512) NOT NULL,
+  `password` varchar(512) NOT NULL,
+  `role` varchar(10) NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+DROP TABLE  IF EXISTS `reminders`;
+CREATE TABLE IF NOT EXISTS `reminders`(
+  `id` int(11) NOT NUll AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  `userid` int(11) NOT NULL,
+  `info` varchar(512) NOT NULL,
+  `date` varchar(20),
+  `time` varchar(20),
+  `location` varchar(512)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+DROP TABLE  IF EXISTS `activities`;
 
 CREATE TABLE `activities` (
   `id` int(1) NOT NULL,
@@ -40,65 +68,36 @@ INSERT INTO `activities` (`id`, `activityName`) VALUES
 (1, 'Outdoor Run'),
 (3, 'Swimming'),
 (4, 'Cycling');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `firstname` varchar(512) NOT NULL,
-  `surname` varchar(512) NOT NULL,
-  `email` varchar(512) NOT NULL,
-  `password` varchar(512) NOT NULL,
-  `role` varchar(10) NOT NULL DEFAULT 'user'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `firstname`, `surname`, `email`, `password`, `role`) VALUES
+ INSERT INTO `users` (`id`, `firstname`, `surname`, `email`, `password`, `role`) VALUES
 (1, 'Kyle', 'McCrear', 'test@gmail.com', 'test', 'user'),
 (2, 'test', 'test', 'test123@gmail.com', '$2b$10$u6dT/I4crqKcrVnum98Dk.gBGWN.dyNquSxK4xWcJtgokfxRodiJ6', 'user'),
 (3, 'Kyle', 'McCrear', 'google@gmail.com', '$2b$10$9lNdystBBA5S7LzHZg33iuTwZEelOck.664jR6mqBCZzjKEsNhI0a', 'user'),
 (6, 'TEst', 'test', 'test@dsfsd', '$2b$10$PsBTvi7Kv7pubDesDxmy2ukoA1C38JTu4FDni1phXVYq2yfcc3J3e', 'user'),
 (7, 'asdasd', '', '', '$2b$10$y7wXJYCZ6lG5EJQQl0K8HOAi20yA1tePHCMnO1vzZp9UWZSq6pLXm', 'user'),
 (12, 'admin', 'admin', 'admin@gcuhealth.co.uk', '$2b$10$neyGefUfXF.7aKtJvVxDD.Amx1D/lTfsS3X5q9r7gq2xw8y9MoBke', 'admin');
-
+INSERT INTO `reminders` (`id`,`userid`, `info`, `date`,`time`,`location`) VALUES (1, 13, 'reminder in db lmao','01/01/2040' ,'4:20pm','idk some st.');
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `activities`
---
-ALTER TABLE `activities`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `users`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `activities`
---
-ALTER TABLE `activities`
-  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  
+ALTER TABLE `activities`
+  ADD PRIMARY KEY (`id`);
+ALTER TABLE `activities`
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
